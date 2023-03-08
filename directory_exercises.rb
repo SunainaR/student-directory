eb7fb200730051579cbb3c90fa166d87c3fa4eaf
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-08 19:29:18 +0000

    Second exercise: start_with? to filter

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -21,7 +21,9 @@ end
 # each_with_index added
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].start_with?("N")
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
