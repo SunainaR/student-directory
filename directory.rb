0497e5e62f7e724a8675bf667d3b4521a7f3cff0
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-09 21:59:29 +0000

    Step 11: student directory saved to csv and gitignore added

--- directory.rb
+++ directory.rb
@@ -1,61 +1,86 @@
+=begin
+Interactive Menu Process:
+1. print the menu and ask the user what to do
+2. read the input and save it to a variable
+3. do what the user asked
+4. repeat from step 1
+=end
+
+# instance variable decalred here so it is accessible in all methods
+@students = []
 def interactive_menu
-  # declared student variable outside loop so persists across several interations of the loop
-  students = []
-  # 1. print the menu and ask the user what to do
-  # 2. read the input and save it to a variable
-  # 3. do what the user asked
-  # 4. repeat from step 1
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        # input students
-        students = input_students
-      when "2"
-        # show students
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit # This will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    # Note that we're passing the user selection as an argument to the method
+    process(gets.chomp)
   end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit # This will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
-
-# Easier to see what is going on with methods
-# Reads more like English
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  # open the file for writing - returns a reference to the file
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    # calling puts on file means will write there and not screen
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
